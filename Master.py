05c6a67a01fbfe3e0c22be0e41207be38a1b39c7
student-DEBANGHSU <personal.debangshu444@gmail.com>
2023-10-10 19:25:20 +0530

    Error Fixed

--- Master.py
+++ Master.py
@@ -12,14 +12,7 @@ print("Welcome to e-Train Reservation System")
 print()
 choice=None
 while choice!=0:
-    choice=int(input("""
-                     1. SIGN UP\n
-                     2. SIGN IN\n
-                     3. ADMIN LOGIN\n
-                     0. EXIT\n\n
-                     Please enter your choice : 
-                     """))
-    
+    choice=int(input("""1. SIGN UP\n2. SIGN IN\n3. ADMIN LOGIN\n0. EXIT\n\nPlease enter your choice : """))
     if choice==0:       # Exit the program
         print("THANK YOU FOR USING e-Train Reservation System")
     elif choice==1:     # Proceed to Sign Up page
